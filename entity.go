727dadb85979fab7c7772655c7f35040c22e8d7e
smiggleworth <jrepanich@gmail.com>
2025-04-03 17:56:49 -0400

    concat entity type and id is problematic with no real value

--- entity.go
+++ entity.go
@@ -1,10 +1,6 @@
 package es
 
 import (
-	"encoding/json"
-	"fmt"
-	"strings"
-
 	"github.com/google/uuid"
 )
 
@@ -29,36 +25,6 @@ var EmptyEntity = Entity{}
 type Entity struct {
 	ID   uuid.UUID `json:"-"`
 	Type string    `json:"-"`
-}
-
-// MarshalJSON converts the entity to the format "type:id".
-func (e *Entity) MarshalJSON() ([]byte, error) {
-	if e.ID == uuid.Nil {
-		return nil, fmt.Errorf("cannot marshal entity with nil ID")
-	}
-	return json.Marshal(fmt.Sprintf("%s:%s", e.Type, e.ID))
-}
-
-// UnmarshalJSON parses the entity from the format "type:id".
-func (e *Entity) UnmarshalJSON(data []byte) error {
-	var raw string
-	if err := json.Unmarshal(data, &raw); err != nil {
-		return err
-	}
-
-	parts := strings.SplitN(raw, ":", 2)
-	if len(parts) != 2 {
-		return fmt.Errorf("invalid entity format: %s", raw)
-	}
-
-	parsedID, err := uuid.Parse(parts[1])
-	if err != nil {
-		return fmt.Errorf("invalid UUID: %w", err)
-	}
-
-	e.Type = parts[0]
-	e.ID = parsedID
-	return nil
 }
 
 // IsEmpty checks if an entity is empty.
